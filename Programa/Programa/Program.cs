71cd305c8740a6fe7051c2395b92cdf863e79909
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-03-04 13:05:30 -0300

    Console.WriteLine adicionado no Program.cs

--- Programa/Programa/Program.cs
+++ Programa/Programa/Program.cs
@@ -15,6 +15,7 @@ namespace Programa
             Console.WriteLine(x / y);
             Console.WriteLine(x ^ 2);
             Console.WriteLine("Resto:" + (x % y));
+            Console.WriteLine();
         }
     }
 }
